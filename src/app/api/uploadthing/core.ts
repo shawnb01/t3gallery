5c33a6de41f011fa3fa90decadc28a226afd58fa
Shawn Bowler <grimtrisharalok@gmail.com>
2024-04-22 21:46:23 -0400

    Lockdown uploads

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { db } from "~/server/db";
@@ -7,17 +7,18 @@ import { ratelimit } from "~/server/ratelimit";
 
 const f = createUploadthing();
 
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 10 } })
-    // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth();
-
-      // If you throw, the user will not be able to upload
+      const user = auth();
       if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      const fullUserData = await clerkClient.users.getUser(user.userId);
+
+      if (!fullUserData.privateMetadata?.["can-upload"] !== true)
+        throw new UploadThingError(
+          "User does not have permission to upload images",
+        );
 
       const { success } = await ratelimit.limit(user.userId);
 
@@ -25,11 +26,9 @@ export const ourFileRouter = {
         throw new UploadThingError("Rate limited");
       }
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.userId };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
 
       await db.insert(images).values({
@@ -38,7 +37,6 @@ export const ourFileRouter = {
         userId: metadata.userId,
       });
 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId };
     }),
 } satisfies FileRouter;
