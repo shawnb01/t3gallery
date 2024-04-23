db9cbd9ca16d6e45a0695cca508d250ffa400d67
Shawn Bowler <grimtrisharalok@gmail.com>
2024-04-22 22:19:29 -0400

    Fixed typo in can-upload check

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -15,7 +15,7 @@ export const ourFileRouter = {
 
       const fullUserData = await clerkClient.users.getUser(user.userId);
 
-      if (!fullUserData.privateMetadata?.["can-upload"] !== true)
+      if (fullUserData.privateMetadata?.["can-upload"] !== true)
         throw new UploadThingError(
           "User does not have permission to upload images",
         );
