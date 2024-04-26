fca5ec14f924b912955f51c237050b754caafee1
Shawn Bowler <grimtrisharalok@gmail.com>
2024-04-25 21:51:32 -0400

    Add X button to img modal

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -25,7 +25,25 @@ export function Modal({ children }: { children: React.ReactNode }) {
       onClose={onDismiss}
     >
       {children}
-      {/* <button onClick={onDismiss} className="close-button" /> */}
+      <button
+        onClick={onDismiss}
+        className="close-button absolute left-4 top-4 rounded-full bg-black/50 p-2 hover:bg-black/70"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
     </dialog>,
     document.getElementById("modal-root")!,
   );
